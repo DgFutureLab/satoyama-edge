6c07734c75845b9b05c73868a1515922bd3356cc
Halfdan Rump <halfdan-rump@garage.co.jp>
2015-10-02 00:21:13 +0900

    Adds new sketch patching previous code together to try to find the place where the program crashes

---
+++ EthernetAggrNew/EthernetAggrNew.ino
@@ -0,0 +1,169 @@
+/*
+  Repeating Web client
+ 
+ This sketch connects to a a web server and makes a request
+ using a Wiznet Ethernet shield. You can use the Arduino Ethernet shield, or
+ the Adafruit Ethernet shield, either one will work, as long as it's got
+ a Wiznet Ethernet module on board.
+ 
+ This example uses DNS, by assigning the Ethernet client with a MAC address,
+ IP address, and DNS address.
+ 
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ 
+ created 19 Apr 2012
+ by Tom Igoe
+ 
+ http://arduino.cc/en/Tutorial/WebClientRepeating
+ This code is in the public domain.
+ 
+ */
+#include <chibi.h>
+#define NODE_ID 1
+#define BUFSIZE 300
+char buf[BUFSIZE];
+ 
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// assign a MAC address for the ethernet controller.
+// fill in your address here:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+// fill in an available IP address on your network here,
+// for manual configuration:
+IPAddress ip(192,168,0,177);
+
+// fill in your Domain Name Server address here:
+
+
+// initialize the library instance:
+EthernetClient client;
+
+char server[] = "satoyamacloud.com";
+
+unsigned long lastConnectionTime = 0;          // last time you connected to the server, in milliseconds
+boolean lastConnected = false;                 // state of the connection last time through the main loop
+const unsigned long postingInterval = 2*1000;  // delay between updates, in milliseconds
+
+void setup() {
+  Serial.begin(115200);
+  // start serial port:
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  chibiInit();
+  chibiSetShortAddr(NODE_ID);
+  
+  Serial.println("Attempting to connect Ethernet using DHCP");
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  Serial.println("Connection established!");
+}
+
+boolean chibi_on = false;
+
+void loop() {
+  // if there's incoming data from the net connection.
+  // send it out the serial port.  This is for debugging
+  // purposes only:
+  
+  if(millis() > 15000){
+    if(!chibi_on){
+    chibi_on = true;
+    Serial.println("**********************");
+    Serial.println("Now listening on Chibi");
+    Serial.println("**********************");
+    }
+    
+    read_chibi(buf);
+    memset(buf, 0, BUFSIZE);
+  }
+  
+  if (client.available()) {
+    Serial.print("*");
+    char c = client.read();
+    Serial.print(c);
+  }
+
+ 
+ 
+ 
+ 
+ 
+  // if there's no net connection, but there was one last time
+  // through the loop, then stop the client:
+  if (!client.connected() && lastConnected) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+
+ 
+ 
+ 
+ 
+  // if you're not connected, and ten seconds have passed since
+  // your last connection, then connect again and send data:
+  if(!client.connected() && (millis() - lastConnectionTime > postingInterval)) {
+    Serial.println("Sending HTTP request");
+    httpRequest();
+  }
+  // store the state of the connection for next time through
+  // the loop:
+  lastConnected = client.connected();
+}
+
+// this method makes a HTTP connection to the server:
+void httpRequest() {
+  // if there's a successful connection:
+  if (client.connect(server, 80)) {
+    Serial.println("connecting...");
+    // send the HTTP PUT request:
+    client.println("GET / HTTP/1.1");
+    client.println("Host: www.arduino.cc");
+    client.println("User-Agent: arduino-ethernet");
+    client.println("Connection: close");
+    client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  } 
+  else {
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
+    Serial.println("disconnecting.");
+    client.stop();
+  }
+}
+
+
+char tmp[100];
+void read_chibi(char *buf){
+  if (chibiDataRcvd() == true){ 
+    Serial.print("1");
+//    int rssi = chibiGetRSSI();
+//    int src_addr = chibiGetSrcAddr();
+    memset(tmp, 0, 100);
+    Serial.print("2");
+    int len = chibiGetData((byte*)tmp);
+    Serial.print("3");
+    Serial.print(" TMPLEN: ");
+    Serial.print(strlen(tmp));
+    Serial.print(" ...");
+    strcat(buf, tmp);
+    Serial.print("4, buf: ");
+    Serial.print(tmp);
+    Serial.print("5FIN");
+  } 
+}
+
+
